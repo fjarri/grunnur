d9f45eed85c31f32735ab9734fa801551b8a7095
Bogdan Opanchuk <bogdan@opanchuk.net>
2020-02-01 20:15:00 -0800

    examples: add an example script

---
+++ examples/multi_device_comparison.py
@@ -0,0 +1,106 @@
+import time
+import numpy
+from grunnur import opencl_api as api
+
+
+src = """
+KERNEL void sum(GLOBAL_MEM unsigned long *a, int pwr)
+{
+    unsigned long m = 4294967231;
+    int gid = get_global_id(0);
+
+    int idx = gid;
+    unsigned long x = a[idx];
+    unsigned long res = x;
+    for (int i = 1; i < pwr; i++)
+    {
+        res *= x;
+        res %= m;
+    }
+    a[idx] = res;
+}
+"""
+
+
+def calc_ref(x, pwr):
+    m = numpy.uint64(2**32-65)
+    res = x.copy()
+    for i in range(1, pwr):
+        res *= x
+        res %= m
+        #res += 1
+    return res
+
+
+def test_single_device(full_len, benchmark=False):
+    pwr = 50
+
+    a = numpy.arange(full_len).astype(numpy.uint64)
+
+    p = api.get_platforms()[0]
+    ds = p.get_devices()
+    ds = [ds[2]]
+
+    context = api.create_context(ds)
+    queue = context.make_queue()
+
+    program = context.compile(src)
+    a_dev = context.upload(queue, a)
+
+    gs = full_len
+    ls = None
+
+    queue.synchronize()
+    t1 = time.time()
+    program.sum(queue, gs, ls, a_dev, numpy.int32(pwr))
+    queue.synchronize()
+    t2 = time.time()
+    print("Single device time:", t2 - t1)
+
+    a_res = a_dev.get()
+
+    if not benchmark:
+        a_ref = calc_ref(a, pwr)
+        assert (a_ref == a_res).all()
+
+
+def test_multi_device(full_len, benchmark=False):
+
+    pwr = 50
+
+    a = numpy.arange(full_len).astype(numpy.uint64)
+
+    p = api.get_platforms()[0]
+    ds = p.get_devices()
+    ds = [ds[1], ds[2]]
+
+    context = api.create_context(ds)
+    queue = context.make_queue()
+
+    program = context.compile(src)
+    a_dev = context.upload(queue, a)
+
+    a_dev_1 = a_dev.single_device_view(0)[:full_len//2]
+    a_dev_2 = a_dev.single_device_view(1)[full_len//2:]
+
+    gs = full_len // 2
+    ls = None
+
+    queue.synchronize()
+    t1 = time.time()
+    program.sum(queue, gs, ls, [a_dev_1, a_dev_2], numpy.int32(pwr), device_nums=[0, 1])
+    queue.synchronize()
+    t2 = time.time()
+    print("Multidevice time:", t2 - t1)
+
+    a_res = a_dev.get()
+
+    if not benchmark:
+        a_ref = calc_ref(a, pwr)
+        assert (a_ref == a_res).all()
+
+test_single_device(2**20)
+test_multi_device(2**20)
+
+test_single_device(2**24, benchmark=True)
+test_multi_device(2**24, benchmark=True)
