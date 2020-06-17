4dc2f88bd406965faa52d58a1ad743e18f58aac4
Bogdan Opanchuk <bogdan@opanchuk.net>
2020-06-17 16:26:10 -0700

    test/mocked/static: add a test for local size finding

--- test/test_mocked/test_static.py
+++ test/test_mocked/test_static.py
@@ -1,3 +1,6 @@
+from grunnur import StaticKernel
+
+from ..mock_base import MockKernel, MockDefTemplate, MockDefTemplate
 from ..test_on_device.test_static import (
     _test_compile_static,
     _test_compile_static_multi_device,
@@ -6,6 +9,14 @@ from ..test_on_device.test_static import (
 
 def test_compile_static(mock_context):
     _test_compile_static(context=mock_context, is_mocked=True)
+
+
+def test_find_local_size(mock_context):
+    kernel = MockKernel('multiply', [None], max_total_local_sizes={0: 64})
+    src = MockDefTemplate(kernels=[kernel])
+    multiply = StaticKernel(mock_context, src, 'multiply', (11, 15))
+    assert multiply._vs_metadata[0].real_global_size == (16, 12)
+    assert multiply._vs_metadata[0].real_local_size == (16, 4)
 
 
 def test_compile_static_multi_device(mock_4_device_context):
