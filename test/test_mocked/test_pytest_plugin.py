2efa10d08d0c2e9d057390d51418c136c93e05a8
Bogdan Opanchuk <bogdan@opanchuk.net>
2020-06-23 20:53:59 -0700

    test/mocked/pytest_plugin: minimizing the amount of tests run in the sandbox

--- test/test_mocked/test_pytest_plugin.py
+++ test/test_mocked/test_pytest_plugin.py
@@ -7,8 +7,7 @@ def run_tests(options=[]):
     pytest.main([
         '-v',
         '--no-cov',
-        "-k", "mocked and pytest_plugin",
-        "-m", "plugin_inner_test"] + options)
+        '-m', 'plugin_inner_test'] + options)
 
 
 @pytest.mark.plugin_inner_test
@@ -29,14 +28,14 @@ def test_api(mock_backend_factory, capsys):
     assert '::test_api_fixture[api(cuda)]' in captured.out
     assert '::test_api_fixture[api(opencl)]' in captured.out
 
-    run_tests(["--api=cuda"])
+    run_tests(['--api=cuda', '-k', 'test_api_fixture'])
     captured = capsys.readouterr()
     assert 'device(cuda,0,0): nVidia CUDA, Device1' in captured.out
     assert 'device(opencl,0,0): Bar, Device2' not in captured.out
     assert '::test_api_fixture[api(cuda)]' in captured.out
     assert '::test_api_fixture[api(opencl)]' not in captured.out
 
-    run_tests(["--api=opencl"])
+    run_tests(['--api=opencl', '-k', 'test_api_fixture'])
     captured = capsys.readouterr()
     assert 'device(cuda,0,0): nVidia CUDA, Device1' not in captured.out
     assert 'device(opencl,0,0): Bar, Device2' in captured.out
@@ -45,7 +44,7 @@ def test_api(mock_backend_factory, capsys):
 
 
 def test_no_api(mock_backend_factory, capsys):
-    run_tests()
+    run_tests(['-k', 'test_api_fixture'])
     captured = capsys.readouterr()
     assert '::test_api_fixture[no_api]' in captured.out
 
@@ -60,21 +59,21 @@ def test_platform(mock_backend_factory, capsys):
     backend_pyopencl.add_platform_with_devices('Foo', ['Device1'])
     backend_pyopencl.add_platform_with_devices('Bar', ['Device2'])
 
-    run_tests()
+    run_tests(['-k', 'test_platform_fixture'])
     captured = capsys.readouterr()
     assert 'device(opencl,0,0): Foo, Device1' in captured.out
     assert 'device(opencl,1,0): Bar, Device2' in captured.out
     assert '::test_platform_fixture[platform(opencl,0)]' in captured.out
     assert '::test_platform_fixture[platform(opencl,1)]' in captured.out
 
-    run_tests(["--platform-include-mask=Bar"])
+    run_tests(['--platform-include-mask=Bar', '-k', 'test_platform_fixture'])
     captured = capsys.readouterr()
     assert 'device(opencl,0,0): Foo, Device1' not in captured.out
     assert 'device(opencl,1,0): Bar, Device2' in captured.out
     assert '::test_platform_fixture[platform(opencl,0)]' not in captured.out
     assert '::test_platform_fixture[platform(opencl,1)]' in captured.out
 
-    run_tests(["--platform-exclude-mask=Bar"])
+    run_tests(['--platform-exclude-mask=Bar', '-k', 'test_platform_fixture'])
     captured = capsys.readouterr()
     assert 'device(opencl,0,0): Foo, Device1' in captured.out
     assert 'device(opencl,1,0): Bar, Device2' not in captured.out
@@ -83,7 +82,7 @@ def test_platform(mock_backend_factory, capsys):
 
 
 def test_no_platform(mock_backend_factory, capsys):
-    run_tests()
+    run_tests(['-k', 'test_platform_fixture'])
     captured = capsys.readouterr()
     assert '::test_platform_fixture[no_platform]' in captured.out
 
@@ -98,7 +97,7 @@ def test_device(mock_backend_factory, capsys):
     backend_pyopencl.add_platform_with_devices('Foo', ['Device1', 'Device2'])
     backend_pyopencl.add_platform_with_devices('Bar', ['Device2', 'Device3'])
 
-    run_tests()
+    run_tests(['-k', 'test_device_fixture'])
     captured = capsys.readouterr()
     assert 'device(opencl,0,0): Foo, Device1' in captured.out
     assert 'device(opencl,0,1): Foo, Device2' in captured.out
@@ -109,7 +108,7 @@ def test_device(mock_backend_factory, capsys):
     assert '::test_device_fixture[device(opencl,1,0)]' in captured.out
     assert '::test_device_fixture[device(opencl,1,1)]' in captured.out
 
-    run_tests(["--device-include-mask=Device2"])
+    run_tests(['--device-include-mask=Device2', '-k', 'test_device_fixture'])
     captured = capsys.readouterr()
     assert 'device(opencl,0,0): Foo, Device1' not in captured.out
     assert 'device(opencl,0,1): Foo, Device2' in captured.out
@@ -120,7 +119,7 @@ def test_device(mock_backend_factory, capsys):
     assert '::test_device_fixture[device(opencl,1,0)]' in captured.out
     assert '::test_device_fixture[device(opencl,1,1)]' not in captured.out
 
-    run_tests(["--device-exclude-mask=Device2"])
+    run_tests(['--device-exclude-mask=Device2', '-k', 'test_device_fixture'])
     captured = capsys.readouterr()
     assert 'device(opencl,0,0): Foo, Device1' in captured.out
     assert 'device(opencl,0,1): Foo, Device2' not in captured.out
@@ -136,7 +135,7 @@ def test_duplicate_devices(mock_backend_factory, capsys):
     backend_pyopencl = mock_backend_factory.mock_pyopencl()
     backend_pyopencl.add_platform_with_devices('Foo', ['Device1', 'Device1', 'Device2'])
 
-    run_tests()
+    run_tests(['-k', 'test_device_fixture'])
     captured = capsys.readouterr()
     assert 'device(opencl,0,0): Foo, Device1' in captured.out
     assert 'device(opencl,0,1): Foo, Device1' not in captured.out
@@ -145,7 +144,7 @@ def test_duplicate_devices(mock_backend_factory, capsys):
     assert '::test_device_fixture[device(opencl,0,1)]' not in captured.out
     assert '::test_device_fixture[device(opencl,0,2)]' in captured.out
 
-    run_tests(["--include-duplicate-devices"])
+    run_tests(['--include-duplicate-devices', '-k', 'test_device_fixture'])
     captured = capsys.readouterr()
     assert 'device(opencl,0,0): Foo, Device1' in captured.out
     assert 'device(opencl,0,1): Foo, Device1' in captured.out
@@ -156,8 +155,9 @@ def test_duplicate_devices(mock_backend_factory, capsys):
 
 
 def test_no_device(mock_backend_factory, capsys):
-    run_tests()
+    run_tests(['-k', 'test_device_fixture'])
     captured = capsys.readouterr()
+    assert 'No GPGPU devices available' in captured.out
     assert '::test_device_fixture[no_device]' in captured.out
 
 
@@ -171,14 +171,14 @@ def test_context(mock_backend_factory, capsys):
     backend_pyopencl = mock_backend_factory.mock_pyopencl()
     backend_pyopencl.add_platform_with_devices('Foo', ['Device1', 'Device2'])
 
-    run_tests()
+    run_tests(['-k', 'test_context_fixture'])
     captured = capsys.readouterr()
     assert 'device(opencl,0,0): Foo, Device1' in captured.out
     assert 'device(opencl,0,1): Foo, Device2' in captured.out
     assert '::test_context_fixture[opencl,0,0]' in captured.out
     assert '::test_context_fixture[opencl,0,1]' in captured.out
 
-    run_tests(['--device-include-mask=Device1'])
+    run_tests(['--device-include-mask=Device1', '-k', 'test_context'])
     captured = capsys.readouterr()
     assert 'device(opencl,0,0): Foo, Device1' in captured.out
     assert 'device(opencl,0,1): Foo, Device2' not in captured.out
@@ -187,7 +187,7 @@ def test_context(mock_backend_factory, capsys):
 
 
 def test_no_context(mock_backend_factory, capsys):
-    run_tests()
+    run_tests(['-k', 'test_context_fixture'])
     captured = capsys.readouterr()
     assert '::test_context_fixture[no_device]' in captured.out
 
@@ -199,15 +199,16 @@ def test_multi_device_context_fixture(multi_device_context):
 
 
 # FIXME: decide on the exact logic in this case.
-def _test_multi_device_context(mock_backend_factory, capsys):
+def test_multi_device_context(mock_backend_factory, capsys):
     backend_pyopencl = mock_backend_factory.mock_pyopencl()
     # Two of the devices have the same names to check that they will be picked up
     backend_pyopencl.add_platform_with_devices('Foo', ['Device1', 'Device1', 'Device3'])
 
-    run_tests()
+    run_tests(['-k', 'test_multi_device_context_fixture'])
     captured = capsys.readouterr()
     assert 'device(opencl,0,0): Foo, Device1' in captured.out
-    assert 'device(opencl,0,1): Foo, Device1' in captured.out
+    # Multi-device context does not currently include all the devices used to the list
+    assert 'device(opencl,0,1): Foo, Device1' not in captured.out
     assert 'device(opencl,0,2): Foo, Device3' in captured.out
     assert '::test_multi_device_context_fixture[opencl,0,0+opencl,0,1+opencl,0,2]' in captured.out
 
@@ -216,6 +217,6 @@ def test_no_multi_device_context(mock_backend_factory, capsys):
     backend_pyopencl = mock_backend_factory.mock_pyopencl()
     backend_pyopencl.add_platform_with_devices('Foo', ['Device1'])
 
-    run_tests()
+    run_tests(['-k', 'test_multi_device_context_fixture'])
     captured = capsys.readouterr()
     assert '::test_multi_device_context_fixture[no_multi_device]' in captured.out
