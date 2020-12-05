9492f057276c762f3e04356b8f93a2dcb7db7de2
Bogdan Opanchuk <bogdan@opanchuk.net>
2020-12-04 21:11:56 -0800

    Add mock_or_real_context fixture and adjust context fixture labels

--- test/conftest.py
+++ test/conftest.py
@@ -2,13 +2,14 @@ import io
 
 import pytest
 
-from grunnur import API, Context, cuda_api_id, opencl_api_id
+from grunnur import API, Context, Device, cuda_api_id, opencl_api_id
 from grunnur.api import all_api_ids
+from grunnur.adapter_base import APIID
 from grunnur.virtual_alloc import TrivialManager, ZeroOffsetManager
 
 # Cannot just use the plugin directly since it is loaded before the coverage plugin,
 # and all the function definitions in all `grunnur` modules get marked as not covered.
-from grunnur.pytest_plugin import context, multi_device_context
+from grunnur.pytest_plugin import context, multi_device_context, get_devices
 from grunnur.pytest_plugin import pytest_addoption as grunnur_pytest_addoption
 from grunnur.pytest_plugin import pytest_generate_tests as grunnur_pytest_generate_tests
 from grunnur.pytest_plugin import pytest_report_header as grunnur_pytest_report_header
@@ -85,9 +86,13 @@ def make_context(backend, devices_num):
     return Context.from_criteria(api, devices_num=devices_num)
 
 
-@pytest.fixture(scope='function')
-def mock_context(request, mock_backend):
-    yield make_context(mock_backend, 1)
+@pytest.fixture(
+    scope='function',
+    params=all_api_ids(),
+    ids=lambda api_id: f"mock_{api_id.shortcut}")
+def mock_context(request, mock_backend_factory):
+    backend = mock_backend_factory.mock(request.param)
+    yield make_context(backend, 1)
 
 
 @pytest.fixture(scope='function')
@@ -95,14 +100,37 @@ def mock_context_pycuda(request, mock_backend_pycuda):
     yield make_context(mock_backend_pycuda, 1)
 
 
-@pytest.fixture(scope='function')
-def mock_4_device_context(request, mock_backend):
-    yield make_context(mock_backend, 4)
+@pytest.fixture(
+    scope='function',
+    params=all_api_ids(),
+    ids=lambda api_id: f"mock_{api_id.shortcut}_4dev")
+def mock_4_device_context(request, mock_backend_factory):
+    backend = mock_backend_factory.mock(request.param)
+    yield make_context(backend, 4)
 
 
 @pytest.fixture(scope='function')
 def mock_4_device_context_pyopencl(request, mock_backend_pyopencl):
     yield make_context(mock_backend_pyopencl, 4)
+
+
+@pytest.fixture(scope='function')
+def mock_or_real_context(request, monkeypatch):
+    # Since `py.test` does not support concatenating fixtures,
+    # we concatenate real contexts and mocked contexts manually.
+    # If there are no devices available, we need it to be noticeable that some tests are skipped,
+    # hence passing `None` and calling `skip()` explicitly in this case.
+    value = request.param
+    if value is None:
+        pytest.skip("No GPGPU devices available")
+    elif isinstance(value, Device):
+        yield (Context.from_devices(value), False)
+    elif isinstance(value, APIID):
+        factory = MockBackendFactory(monkeypatch)
+        backend = factory.mock(value)
+        yield (make_context(backend, 1), True)
+    else:
+        raise TypeError
 
 
 class MockStdin:
@@ -146,8 +174,27 @@ def pytest_addoption(parser):
     grunnur_pytest_addoption(parser)
 
 
+def _mock_or_real_context_make_id(value):
+    if value is None:
+        return "no_real_devices"
+    elif isinstance(value, Device):
+        return value.shortcut
+    elif isinstance(value, APIID):
+        return f"mock_{value.shortcut}"
+    else:
+        raise TypeError
+
+
 def pytest_generate_tests(metafunc):
     grunnur_pytest_generate_tests(metafunc)
+
+    devices = get_devices(metafunc.config)
+    api_ids = all_api_ids()
+
+    if 'mock_or_real_context' in metafunc.fixturenames:
+        values = (devices if len(devices) > 0 else [None]) + all_api_ids()
+        ids = [_mock_or_real_context_make_id(value) for value in values]
+        metafunc.parametrize('mock_or_real_context', values, ids=ids, indirect=True)
 
 
 def pytest_report_header(config):
